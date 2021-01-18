f193a42659ec2bde7e6cfb35abaa8d94f1ee0c07
rashidat <rasidatb@gmail.com>
2021-01-18 11:54:56 +0000

    adding database

---
+++ Gemfile
@@ -0,0 +1,16 @@
+
+
+source "https://rubygems.org"
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem 'capybara'
+gem 'pg'
+gem 'rspec'
+gem 'sinatra'
+
+group :development, :test do
+gem "rubocop", "0.79.0" 
+end
+
+
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra/base'
+require './lib/bookmark'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/' do
+      'BookmarkManager'
+  end
+
+  get '/bookmarks' do
+    p ENV
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end
+
+run! if app_file == $0
+end
+
+
